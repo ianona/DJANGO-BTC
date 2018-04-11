08e970d80609c0c800bf675f9d9ac855d04ed617
Ian Ona <ianbenedictona@gmail.com>
2018-04-12 01:07:10 +0800

    final

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^intro/$', views.intro, name='intro'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^q1/$', views.q1, name='q1'),
+    url(r'^q2/$', views.q2, name='q2'),
+    url(r'^q3/$', views.q3, name='q3'),
+    url(r'^q4/$', views.q4, name='q4'),
+    url(r'^conclusion/$', views.conclusion, name='conclusion'),
 ]
-
+urlpatterns += staticfiles_urlpatterns()
--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,21 @@ def index(request):
 def intro(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/intro.html', {'posts': posts})
+
+def about(request):
+	return render(request, 'blog/about.html')
+
+def q1(request):
+	return render(request, 'blog/q1.html')
+
+def q2(request):
+	return render(request, 'blog/q2.html')
+
+def q3(request):
+	return render(request, 'blog/q3.html')
+
+def q4(request):
+	return render(request, 'blog/q4.html')
+
+def conclusion(request):
+	return render(request, 'blog/conclusion.html')
